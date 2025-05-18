9db9f3adefe88300e2860e5a93782e18356ec116
Marco Portacio <marcoportacio4@gmail.com>
2025-05-18 17:34:20 -0500

    fix: possible crash when places result was undefined

--- store/search/thunks.ts
+++ store/search/thunks.ts
@@ -1,4 +1,5 @@
 import { GooglePlacesApi, PlacesResponse } from '@/api'
+import { Alert } from 'react-native'
 import { AppDispatch } from '../store'
 import { setLoading, setPlaces } from './searchSlice'
 
@@ -12,6 +13,13 @@ export const searchPlace = (query: string) => {
           textQuery: `${query} monteria`,
         },
       )
+
+      if (!result.data.places) {
+        Alert.alert('No se encontraron resultados')
+        dispatch(setPlaces([]))
+        return
+      }
+
       dispatch(setPlaces(result.data.places))
     } catch (error) {
       console.log(error)
