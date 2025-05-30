ffea9f7dea8511358ac3ea16134e244402dc7bce
Marco Portacio <marcoportacio4@gmail.com>
2025-05-29 22:49:58 -0500

    feat: se añadió el componente GestureHandlerRootView en Home y se mejoró el estilo del ParkingDetailsSheet con un nuevo color de fondo

--- modules/home/components/ParkingDetailsSheet.tsx
+++ modules/home/components/ParkingDetailsSheet.tsx
@@ -227,12 +227,15 @@ export const ParkingDetailsSheet = forwardRef<BottomSheet, Props>(
         snapPoints={snapPoints}
         onChange={onChange}
         handleComponent={() => (
-          <HStack className="items-center justify-center w-full">
+          <HStack
+            className="items-center justify-center w-full bg-[#FAF9F6]"
+            style={{ borderTopLeftRadius: 50, borderTopRightRadius: 50 }}
+          >
             <Icon as={ChevronsDown} className="w-9 h-9" />
           </HStack>
         )}
         style={{
-          backgroundColor: '#f3f4f6',
+          backgroundColor: '#FAF9F6',
           borderTopLeftRadius: 50,
           borderTopRightRadius: 50,
         }}
@@ -249,6 +252,7 @@ const styles = StyleSheet.create({
   contentContainer: {
     padding: 24,
     paddingBottom: 100, // Padding adicional en la parte inferior
+    backgroundColor: '#FAF9F6',
   },
   scrollViewContent: {
     flexGrow: 1,
